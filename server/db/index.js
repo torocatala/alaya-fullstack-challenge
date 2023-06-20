1388bc3e0ea9680c8abbb41167252bf6e7cd54aa
Ivan Jimenez <torocatala@gmail.com>
2023-06-20 03:29:17 +0200

    Add working db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/mern-stack', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect('mongodb://admin:secret@mongo:27017/admin', { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 1000 })
     .catch(e => {
         console.error('Connection error', e.message)
     });
